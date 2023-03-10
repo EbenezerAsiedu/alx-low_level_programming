76352b46de0241dd0e909f57bd7ee9f6aa7c2c69
Ebenezer Asiedu <asieduebenezer1@outlook.com>
2023-03-10 12:39:25 -0800

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * _puts - writes a line of string to the output
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *s);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
