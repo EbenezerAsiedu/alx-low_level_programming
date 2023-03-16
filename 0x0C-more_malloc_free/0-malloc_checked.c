27198858eb12e899d6ec0055f33dc76d35eece3b
Ebenezer Asiedu <asieduebenezer1@outlook.com>
2023-03-16 10:18:13 -0700

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,27 +6,16 @@
  * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
-
  */
-
 void *malloc_checked(unsigned int b)
 
 {
 
 	void *ptr;
 
-
-
 	ptr = malloc(b);
 
-
-
 	if (ptr == NULL)
-
 		exit(98);
-
-
-
 	return (ptr);
-
 }
